aff997c8279d800ac7c00d4c2b62051b5bd34deb
Lookins01 <lookins1q@gmail.com>
2025-01-17 16:43:42 +0500

    [build] add `--debug` flag to nob

--- nob.c
+++ nob.c
@@ -41,9 +41,20 @@ bool generate_words_header()
 }
 
 
-int main(int arc, char **argv)
+int main(int argc, char **argv)
 {
-    NOB_GO_REBUILD_URSELF(arc, argv);
+    NOB_GO_REBUILD_URSELF(argc, argv);
+
+    bool debug = false;
+
+    (void) shift_args(&argc, &argv); // Skip program name
+    while (argc > 0) {
+        char *arg = shift_args(&argc, &argv);
+        if (strcmp(arg, "--debug") == 0) {
+            debug = true;
+        }
+    }
+
     mkdir_if_not_exists("./build/");
     mkdir_if_not_exists("./wasm/");
     Cmd cmd = {0};
@@ -52,26 +63,34 @@ int main(int arc, char **argv)
     if (!generate_words_header(&cmd)) return 1;
 
     /* Compile wordle for linux */
-    if (!needs_rebuild1("./build/wordle", SOURCE_FILE_PATH)) return 0;
-    cmd_append(&cmd, "clang", CFLAGS);
-    cmd_append(&cmd, "-I./build/");
-    cmd_append(&cmd, "-I./");
-    cmd_append(&cmd, "-I./raylib/raylib-5.5_linux_amd64/include");
-    cmd_append(&cmd, "-o", "./build/wordle", SOURCE_FILE_PATH);
-    cmd_append(&cmd, "-L./raylib/raylib-5.5_linux_amd64/lib");
-    cmd_append(&cmd, "-lraylib");
-    if (!cmd_run_sync_and_reset(&cmd)) return 1;
+    if (debug || needs_rebuild1("./build/wordle", SOURCE_FILE_PATH) == 1) {
+        cmd_append(&cmd, "clang", CFLAGS);
+        cmd_append(&cmd, "-I./build/");
+        cmd_append(&cmd, "-I./");
+        cmd_append(&cmd, "-I./raylib/raylib-5.5_linux_amd64/include");
+        cmd_append(&cmd, "-o", "./build/wordle", SOURCE_FILE_PATH);
+        cmd_append(&cmd, "-L./raylib/raylib-5.5_linux_amd64/lib");
+        cmd_append(&cmd, "-lraylib");
+        if (debug) cmd_append(&cmd, "-DDEBUG");
+        if (!cmd_run_sync_and_reset(&cmd)) return 1;
+    } else {
+        nob_log(NOB_INFO, "'./build/wordle' is up to date. ");
+    }
 
     /* Compile wordle for wasm */
-    if (!needs_rebuild1("./wasm/wordle.wasm", SOURCE_FILE_PATH)) return 0;
-    cmd_append(&cmd, "clang", CFLAGS);
-    cmd_append(&cmd, "--target=wasm32", "--no-standard-libraries", "-Wl,--export-table", "-Wl,--no-entry", "-Wl,--allow-undefined", "-Wl,--export=main", "-Wl,--export=__head_base", "-Wl,--allow-undefined");
-    cmd_append(&cmd, "-I./build/");
-    cmd_append(&cmd, "-I./");
-    cmd_append(&cmd, "-I./include");
-    cmd_append(&cmd, "-I./raylib/raylib-5.5_linux_amd64/include");
-    cmd_append(&cmd, "-o", "./wasm/wordle.wasm", SOURCE_FILE_PATH, "-DPLATFORM_WEB");
-    if (!cmd_run_sync_and_reset(&cmd)) return 1;
+    if (debug || needs_rebuild1("./wasm/wordle.wasm", SOURCE_FILE_PATH) == 1) {
+        cmd_append(&cmd, "clang", CFLAGS);
+        cmd_append(&cmd, "--target=wasm32", "--no-standard-libraries", "-Wl,--export-table", "-Wl,--no-entry", "-Wl,--allow-undefined", "-Wl,--export=main", "-Wl,--export=__head_base", "-Wl,--allow-undefined");
+        cmd_append(&cmd, "-I./build/");
+        cmd_append(&cmd, "-I./");
+        cmd_append(&cmd, "-I./include");
+        cmd_append(&cmd, "-I./raylib/raylib-5.5_linux_amd64/include");
+        cmd_append(&cmd, "-o", "./wasm/wordle.wasm", SOURCE_FILE_PATH, "-DPLATFORM_WEB");
+        if (debug) cmd_append(&cmd, "-DDEBUG");
+        if (!cmd_run_sync_and_reset(&cmd)) return 1;
+    } else {
+        nob_log(NOB_INFO, "'./wasm/wordle.wasm' is up to date. ");
+    }
 
     return 0;
 }
